76c2518920448fb2328e01649c167d22b85d3d36
rodrixs054 <rodri77754@gmail.com>
2024-09-17 14:17:25 -0600

    Modificación de html, animación css y javaScript

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,18 +1,15 @@
-/*Animación para la sección de mis proyectos realizados*/
 document.addEventListener('DOMContentLoaded', function() {
   const observerOptions = {
     root: null,
     rootMargin: '0px',
-    threshold: 0.1 // El elemento se anima cuando el 10% de su altura es visible
+    threshold: 0.1
   };
 
   const observer = new IntersectionObserver((entries, observer) => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
         entry.target.classList.add('animate');
-        observer.unobserve(entry.target); // Deja de observar el elemento una vez animado
-      }else{
-        entry.target.classList.remove('animate');
+        observer.unobserve(entry.target); 
       }
     });
   }, observerOptions);
@@ -20,6 +17,5 @@ document.addEventListener('DOMContentLoaded', function() {
   const elementsToAnimate = document.querySelectorAll('.animate-on-scroll');
   elementsToAnimate.forEach(element => {
     observer.observe(element);
-
   });
 });
